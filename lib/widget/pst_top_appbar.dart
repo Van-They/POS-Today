2eb2049817925c13c2a39d54e577c7c5b79c4041
Vann <tvanthey@gmail.com>
2025-05-05 19:34:51 +0700

    update gradle

--- lib/widget/pst_top_appbar.dart
+++ lib/widget/pst_top_appbar.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class PstTopAppbar extends StatelessWidget implements PreferredSizeWidget {
-  const PstTopAppbar ({super.key});
+  final String title;
+  final bool automaticallyImplyLeading;
+  const PstTopAppbar({
+    this.title = "",
+    this.automaticallyImplyLeading = false,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final currentRoute = GoRouter.of(context).routeInformationProvider.value.uri.toString();
+    return AppBar(
+      title: Text(title),
+      automaticallyImplyLeading: automaticallyImplyLeading,
+      leading: IconButton(onPressed: () {
+       //todo finish current activity
+       context.pop(currentRoute);
+      }, icon: Icon(Icons.arrow_back_rounded)),
+    );
   }
-  
+
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight) ;
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
